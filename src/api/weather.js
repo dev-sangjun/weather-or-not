3b3648ff258962ebd8e1767e1b17fd7651d4df8c
ohyeyoye <33212292+ohyeyoye@users.noreply.github.com>
2020-05-29 09:29:16 +0900

    api call updated

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
-
-export const getWeather = coordinates => {
-  const url = `https://api.openweathermap.org/data/2.5/onecall`;
-  const { lon, lat } = coordinates;
+export const getWeather = ({ lat, lon }) => {
+  const url = "https://weather-or-not-server.herokuapp.com/weather";
   const config = {
     params: {
       lat,
       lon,
-      units: "metric",
-      exclude: "hourly",
-      appid: API_KEY,
     },
   };
   return axios.get(url, config);
